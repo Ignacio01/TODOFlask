1e4309f77b320053db4acd7eee7a1fc356b414bd
Ignacio Ojanguren <ignacio@Ignacios-MacBook-Pro.local>
2018-02-25 22:59:09 -0500

    Added updating values

--- app/routes.py
+++ app/routes.py
@@ -64,6 +64,8 @@ def create_task():
     http://localhost:5000/
     :return:
     """
+
+    # Retrieving the current values in the database to show all the tasks stored
     tasks_db = Task.query.all()
     tasks = []
 
@@ -109,27 +111,34 @@ def update_task(task_id):
     :param task_id:
     :return:
     """
-    task = [task for task in tasks if task['id'] == task_id]
+
+    data = request.json
 
     # Validate that the fields to be updated are not incorrect
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
+    if not data:
         abort(400)
     # unicode not working
-    if 'title' in request.json and type(request.json['title']) != str:
+    if 'title' in data and type(data['title']) != str:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) != str:
+    if 'description' in data and type(data['description']) != str:
         abort(400)
-    if 'done' in request.json and type(request.json['done']) != bool:
+    if 'done' in data and type(data['done']) != bool:
         abort(400)
 
-    # Update the values in the array task
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
+    task = Task.query.get(task_id)
 
-    return jsonify({'task': task[0]})
+    task.title = data.get('title', task.title)
+    task.description = data.get('description', task.description)
+    task.done = data.get('done', task.done)
+
+    db.session.commit()
+
+    return jsonify({'task': {
+        'id': task.id,
+        'title': task.title,
+        'description': task.description,
+        'done': task.done
+    }})
 
 
 @app.route('/<int:task_id>', methods=['DELETE'])
